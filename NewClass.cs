650d974e026008c2615615685cecc04dc3882219
dgabel01 <dgabel01@fesb.hr>
2023-03-15 19:16:02 +0100

    Add newclass

---
+++ NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DG_PIS_LAB1
+{
+    class NewClass
+    {
+    }
+}
